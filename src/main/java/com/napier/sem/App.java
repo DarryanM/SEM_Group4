201bdae707194860dfc32d54cead0c2342c97193
Darryan Mathurin <40665392@live.napier.ac.uk>
2024-03-03 21:07:37 -0400

    All Country Population from Largest To Smallest

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,7 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class App {
 
@@ -10,6 +11,12 @@ public class App {
 
         // Connect to database
         a.connect();
+
+        // Extract Country Population
+        ArrayList<Country> population = a.getCountryPopulation();
+
+        //Display Results
+        a.printCountryPopulation(population);
 
 
         // Disconnect from database
@@ -53,7 +60,6 @@ public class App {
     }
 
 
-
     /**
      * Disconnect from the MySQL database.
      */
@@ -68,4 +74,60 @@ public class App {
         }
     }
 
+    /**
+     * Gets the population of all countries.
+     * @return A list of all Population sorted in descending order, or null if there is an error.
+     */
+    public ArrayList<Country> getCountryPopulation()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT name, continent, Region, population "
+                            + "FROM country "
+                            + "Order By population DESC "
+                            + "Limit 10";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract Population information
+            ArrayList<Country> population = new ArrayList<Country>();
+            while (rset.next())
+            {
+                Country pop = new Country();
+                pop.population = rset.getInt("country.population");
+                pop.name = rset.getString("country.Name");
+                pop.continent = rset.getString("country.continent");
+                pop.region = rset.getString("country.region");
+                population.add(pop);
+            }
+            return population;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get Population details");
+            return null;
+        }
+    }
+    /**
+     * Prints a list of Populations.
+     * @param population The list of Population to print.
+     */
+    public void printCountryPopulation(ArrayList<Country> population)
+    {
+        // Print header
+        System.out.println(String.format("%-20s ", "All the countries in the world organised by largest population to smallest."));
+        System.out.println(String.format("%-20s ", " "));
+        System.out.println(String.format("%-20s %-20s %-30s %10s", "Country", "Continent", "Region", "Population"));
+        // Loop over all Retrieved Populations in the list
+        for (Country pop : population)
+        {
+
+            String popCount = String.format("%-20s %-20s %-30s %10s", pop.name, pop.continent, pop.region, pop.population);
+            System.out.println(popCount);
+        }
+    }
 }
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,14 @@
+package com.napier.sem;
+
+public class Country {
+    /**
+     * World Population
+     */
+    public int population;
+    /**
+     * Country Names
+     */
+    public String name;
+    public String continent;
+    public String region;
+}
