b72229105af4e16fe4e275567b6657f05e2e353e
ShalexBailey <40679456@live.napier.ac.uk>
2024-03-03 22:28:50 -0400

    Added comments to code

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -199,6 +199,10 @@ public class App {
             System.out.println(popCount);
         }
     }
+
+    /**
+     * Gets All the Cities in a Country.
+     */
     public ArrayList<City> getCityPopulation10()
     {
         try
@@ -235,11 +239,11 @@ public class App {
             return null;
         }
     }
+
     /**
-     * Prints a list of Populations.
+     * Prints a list of all the Cities in a country.
 
      */
-
 
     public void printCityPopulation10(ArrayList<City> citypop10)
     {
