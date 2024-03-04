c59abf975f0fa7af2148e9f6fa3757347062dd1a
Darren Griffith <40683193@live.napier.ac.uk>
2024-03-03 22:29:36 -0400

    Created Top N Countries Branch.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -30,7 +30,11 @@ public class App {
         //Display Results
         a.printCityPopulation10(citypop10);
 
+        // Extract Top Countries Population
+        ArrayList<Country> topPopulation = a.getTopNCountryPopulation();
 
+        //Display Results
+        a.printTopNCountryPopulation(topPopulation);
 
         // Disconnect from database
         a.disconnect();
@@ -251,6 +255,59 @@ public class App {
         for (City pop : citypop10)
         {
             String popCount = String.format("%-20s %-20s %-30s", pop.name, pop.country, pop.population);
+            System.out.println(popCount);
+        }
+    }
+
+    /**
+     * Gets the population of Top N of all countries in the world.
+     *
+     * @return A list of all Top Populated countries, or null if there is an error.
+     */
+    public ArrayList<Country> getTopNCountryPopulation() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect = "SELECT name, continent, Region, population "
+                    + "FROM country "
+                    + "Order By population DESC "
+                    + "Limit 10";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract Population information
+            ArrayList<Country> population = new ArrayList<Country>();
+            while (rset.next()) {
+                Country pop = new Country();
+                pop.population = rset.getInt("country.population");
+                pop.name = rset.getString("country.Name");
+                pop.continent = rset.getString("country.continent");
+                pop.region = rset.getString("country.region");
+                population.add(pop);
+            }
+            return population;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get Population details");
+            return null;
+        }
+    }
+    /**
+     * Prints a list of Top N Populations.
+     * @param topPopulation The list of Population to print.
+     */
+    public void printTopNCountryPopulation (ArrayList < Country > topPopulation)
+    {
+        // Print header
+        System.out.println(String.format("%-20s ", " "));
+        System.out.println(String.format("%-20s ", "All the Top N countries in the world."));
+        System.out.println(String.format("%-20s ", " "));
+        System.out.println(String.format("%-20s %-20s %-30s %10s", "Country", "Continent", "Region", "Population"));
+        // Loop over all Retrieved Populations in the list
+        for (Country pop : topPopulation) {
+
+            String popCount = String.format("%-20s %-20s %-30s %10s", pop.name, pop.continent, pop.region, pop.population);
             System.out.println(popCount);
         }
     }
