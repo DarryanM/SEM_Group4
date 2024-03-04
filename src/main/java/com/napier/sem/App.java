3e408cab84c9b526560bb70c424b11c9580bfb76
KHiplyte <40715280@live.napier.ac.uk>
2024-03-04 00:02:01 -0400

    Final code of 1st sprint

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -36,6 +36,12 @@ public class App {
         //Display Results
         a.printTopNCountryPopulation(topPopulation);
 
+        // Extract Top City Population in a Continent
+        ArrayList<City> nCityPop = a.getTopCityPopulation();
+
+        //Display Results
+        a.printTopCityPopulation(nCityPop);
+
         // Disconnect from database
         a.disconnect();
     }
@@ -93,12 +99,11 @@ public class App {
 
     /**
      * Gets the population of all countries.
+     *
      * @return A list of all Population sorted in descending order, or null if there is an error.
      */
-    public ArrayList<Country> getCountryPopulation()
-    {
-        try
-        {
+    public ArrayList<Country> getCountryPopulation() {
+        try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
@@ -110,8 +115,7 @@ public class App {
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract Population information
             ArrayList<Country> population = new ArrayList<Country>();
-            while (rset.next())
-            {
+            while (rset.next()) {
                 Country pop = new Country();
                 pop.population = rset.getInt("country.population");
                 pop.name = rset.getString("country.Name");
@@ -120,40 +124,38 @@ public class App {
                 population.add(pop);
             }
             return population;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get Population details");
             return null;
         }
     }
+
     /**
      * Prints a list of Populations.
+     *
      * @param population The list of Population to print.
      */
-    public void printCountryPopulation(ArrayList<Country> population)
-    {
+    public void printCountryPopulation(ArrayList<Country> population) {
         // Print header
         System.out.println(String.format("%-20s ", "All the countries in the world organised by largest population to smallest."));
         System.out.println(String.format("%-20s ", " "));
         System.out.println(String.format("%-20s %-20s %-30s %10s", "Country", "Continent", "Region", "Population"));
         // Loop over all Retrieved Populations in the list
-        for (Country pop : population)
-        {
+        for (Country pop : population) {
 
             String popCount = String.format("%-20s %-20s %-30s %10s", pop.name, pop.continent, pop.region, pop.population);
             System.out.println(popCount);
         }
     }
+
     /**
      * Gets the All Cities population in the world .
+     *
      * @return A list of all cities in the world Population sorted in descending order, or null if there is an error.
      */
-    public ArrayList<City> getCityPop()
-    {
-        try
-        {
+    public ArrayList<City> getCityPop() {
+        try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
@@ -166,8 +168,7 @@ public class App {
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract Population information
             ArrayList<City> cityPop = new ArrayList<City>();
-            while (rset.next())
-            {
+            while (rset.next()) {
                 City pop = new City();
                 pop.population = rset.getInt("city.population");
                 pop.name = rset.getString("city.Name");
@@ -176,28 +177,26 @@ public class App {
                 cityPop.add(pop);
             }
             return cityPop;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get Population details");
             return null;
         }
     }
+
     /**
      * Prints a list of All the Cities in the World.
+     *
      * @param CityPop The list of All Cities in the world Population to print.
      */
-    public void printCityPop(ArrayList<City> CityPop)
-    {
+    public void printCityPop(ArrayList<City> CityPop) {
         // Print header
         System.out.println(String.format("%-20s ", " "));
         System.out.println(String.format("%-20s ", "All the Cities in the world organised by largest population to smallest."));
         System.out.println(String.format("%-20s ", " "));
         System.out.println(String.format("%-20s %-20s %-30s %10s", "Name", "Country Code", "District", "Population"));
         // Loop over all Retrieved Populations in the list
-        for (City pop : CityPop)
-        {
+        for (City pop : CityPop) {
 
             String popCount = String.format("%-20s %-20s %-30s %10s", pop.name, pop.countryCode, pop.district, pop.population);
             System.out.println(popCount);
@@ -207,10 +206,8 @@ public class App {
     /**
      * Gets All the Cities in a Country.
      */
-    public ArrayList<City> getCityPopulation10()
-    {
-        try
-        {
+    public ArrayList<City> getCityPopulation10() {
+        try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
@@ -225,8 +222,7 @@ public class App {
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract Population information
             ArrayList<City> population = new ArrayList<>();
-            while (rset.next())
-            {
+            while (rset.next()) {
                 City citypop10 = new City();
                 citypop10.population = rset.getInt("city.population");
                 citypop10.name = rset.getString("city");
@@ -235,9 +231,7 @@ public class App {
                 population.add(citypop10);
             }
             return population;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get Population details");
             return null;
@@ -246,18 +240,15 @@ public class App {
 
     /**
      * Prints a list of all the Cities in a country.
-
      */
 
-    public void printCityPopulation10(ArrayList<City> citypop10)
-    {
+    public void printCityPopulation10(ArrayList<City> citypop10) {
         // Print header
         System.out.println(String.format("%-20s ", " "));
         System.out.println(String.format("Cities in a country from largest to smallest population"));
         System.out.println(String.format("%-20s %-20s %-30s", "Name", "Country", "Population"));
         // Loop over all Retrieved Populations in the list
-        for (City pop : citypop10)
-        {
+        for (City pop : citypop10) {
             String popCount = String.format("%-20s %-20s %-30s", pop.name, pop.country, pop.population);
             System.out.println(popCount);
         }
@@ -297,12 +288,13 @@ public class App {
             return null;
         }
     }
+
     /**
      * Prints a list of Top N Populations.
+     *
      * @param topPopulation The list of Population to print.
      */
-    public void printTopNCountryPopulation (ArrayList < Country > topPopulation)
-    {
+    public void printTopNCountryPopulation(ArrayList<Country> topPopulation) {
         // Print header
         System.out.println(String.format("%-20s ", " "));
         System.out.println(String.format("%-20s ", "All the Top N countries in the world."));
@@ -312,6 +304,56 @@ public class App {
         for (Country pop : topPopulation) {
 
             String popCount = String.format("%-20s %-20s %-30s %10s", pop.name, pop.continent, pop.region, pop.population);
+            System.out.println(popCount);
+        }
+    }
+
+    public ArrayList<City> getTopCityPopulation() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+
+                    "WITH city1 as (select city.name as name, country.continent as continent, city.population as population, RANK () " +
+                            "OVER(PARTITION BY continent ORDER BY population DESC) row_num " +
+                            "FROM city inner join country on city.countrycode = country.code) " +
+                            "SELECT * FROM city1  WHERE row_num <=3";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract Population information
+            ArrayList<City> nCityPop = new ArrayList<City>();
+            while (rset.next()) {
+                City pop = new City();
+                pop.population = rset.getInt("population");
+                pop.name = rset.getString("Name");
+                pop.continent = rset.getString("continent");
+                pop.row_num = rset.getInt("row_num");
+                nCityPop.add(pop);
+            }
+            return nCityPop;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get Population details");
+            return null;
+        }
+    }
+
+    /**
+     * Prints a list of Populations.
+     *
+     * @param nCityPop The list of Population to print.
+     */
+    public void printTopCityPopulation(ArrayList<City> nCityPop) {
+        // Print header
+        System.out.println(String.format("%-20s ", "The top N populated cities in a continent where N is provided by the user."));
+        System.out.println(String.format("%-20s ", " "));
+        System.out.println(String.format("%10s %-30s %-30s %10s", "row_num", "Country", "Continent", "Population"));
+        // Loop over all Retrieved Populations in the list
+        for (City pop : nCityPop) {
+
+            String popCount = String.format("%10s %-30s %-30s %10s", pop.row_num, pop.name, pop.continent, pop.population);
             System.out.println(popCount);
         }
     }
