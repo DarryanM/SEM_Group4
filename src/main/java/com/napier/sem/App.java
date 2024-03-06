e27c9e72001243c50369efc27597cc059b95cfe6
Darren Griffith <40683193@live.napier.ac.uk>
2024-03-05 23:27:03 -0400

    Creation of Top N Countries in a Region Branch and Report.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -55,10 +55,10 @@ public class App {
         a.printCityPop8(cityPop8);
 
         // Extract Top N Countries in a Continent
-        ArrayList<Country> population5 = a.getTopNCountriesInContPopulation();
+        ArrayList<Country> topNCountriesContPop = a.getTopNCountriesInContPopulation();
 
         //Display Results
-        a.printTopNCountriesInContPopulation(population5);
+        a.printTopNCountriesInContPopulation(topNCountriesContPop);
 
         // Extract district population information
         ArrayList<City> districtPopulation11 = a.getDistrictPopulation();
@@ -89,6 +89,12 @@ public class App {
 
         //Display Results for city population in a region
         a.printCityPop9(cityPop9);
+
+        // Extract Top N Countries in a Region
+        ArrayList<Country> topNCountriesRegPop = a.getTopNCountriesInRegPopulation();
+
+        //Display Results
+        a.printTopNCountriesInRegPopulation(topNCountriesRegPop);
 
         // Disconnect from database
         a.disconnect();
@@ -860,5 +866,64 @@ public class App {
             String popCount = String.format("%-20s %-20s %-20s %-30s %10s", pop.countryCode, pop.name, pop.country, pop.region, pop.population);
             System.out.println(popCount);
         }
+    }
+
+    /**
+     * Gets the all the countries in a Region.
+     *
+     * @return A list of Top N Countries Population in a Region with value provide by user, or null if there is an error.
+     */
+    public ArrayList<Country> getTopNCountriesInRegPopulation() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+
+                    "with country as (select name, code, capital, region, continent, population, row_number() over " +
+                            "(partition by region order by population desc, name desc) as row_num from country) " +
+                            "select row_num, name, code, capital, region, continent, population from country where row_num <=3";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract Population information
+            ArrayList<Country> topNCountriesRegPop = new ArrayList<Country>();
+            while (rset.next()) {
+                Country pop = new Country();
+                pop.population = rset.getInt("country.population");
+                pop.code = rset.getString("country.Code");
+                pop.capital = rset.getInt("country.Capital");
+                pop.name = rset.getString("country.Name");
+                pop.continent = rset.getString("country.continent");
+                pop.region = rset.getString("country.region");
+                pop.row_num = rset.getInt("country.row_num");
+                topNCountriesRegPop.add(pop);
+            }
+            return topNCountriesRegPop;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get Population details");
+            return null;
+        }
+    }
+
+    /**
+     * Prints a list of Top N Countries Populations in a Region.
+     *
+     * @param topNCountriesRegPop The list of Top N Countries in a Region to print.
+     */
+    public void printTopNCountriesInRegPopulation(ArrayList<Country> topNCountriesRegPop) {
+        // Print header
+        System.out.println(String.format("%-20s ", " "));
+        System.out.println(String.format("%-20s ", "All the TOP N countries in a Region with N value provided by user."));
+        System.out.println(String.format("%-20s ", " "));
+        System.out.println(String.format("%-10s %-10s %10s %-50s %-20s %-25s %-30s", "row_num", "Code", "Population", "Country", "Capital", "Continent", "Region"));
+        // Loop over all Retrieved Populations in the list
+        for (Country pop : topNCountriesRegPop) {
+
+            String popCount = String.format("%-10s %-10s %10s %-50s %-20s %-25s %-30s",pop.row_num, pop.code, pop.population, pop.name, pop.capital, pop.continent, pop.region);
+            System.out.println(popCount);
+        }
+
     }
 }
