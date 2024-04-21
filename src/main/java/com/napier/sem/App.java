4740d5e759d9fc55400b6a4590ed269860d1c18f
Darryan Mathurin <40665392@live.napier.ac.uk>
2024-04-21 18:33:36 -0400

    Unit Testing For Reports 1 - 3

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -12,6 +12,24 @@ public class App {
         // Connect to database
         a.connect();
 
+        // Extract Country Population
+        ArrayList<Country> population = a.getCountryPopulation();
+
+        //Display Results
+        a.printCountryPopulation(population);
+
+        // Extract Continent Population
+        ArrayList<Country> population2 = a.getContinentPopulation("Europe");
+
+        //Display Continent Population Results
+        a.printContinentPopulation(population2);
+
+        // Extract Region Population
+        ArrayList<Country> population3 = a.getRegionPopulation("Caribbean");
+
+        //Display Region Population Results
+        a.printRegionPopulation(population3);
+
         // Extract Top Countries Population
         ArrayList<Country> topPopulation = a.getTopNCountryPopulation(10);
 
@@ -81,6 +99,210 @@ public class App {
             }
         }
     }
+
+
+    /**
+     * Gets the population of all countries.
+     *
+     * @return A list of all Population sorted in descending order, or null if there is an error.
+     */
+    public ArrayList<Country> getCountryPopulation() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT code, name, continent, region, capital, population "
+                            + "FROM country "
+                            + "Order By population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract Population information
+            ArrayList<Country> population = new ArrayList<Country>();
+            while (rset.next()) {
+                Country pop = new Country();
+                pop.population = rset.getInt("country.population");
+                pop.name = rset.getString("country.Name");
+                pop.continent = rset.getString("country.continent");
+                pop.capital = rset.getInt("country.capital");
+                pop.region = rset.getString("country.region");
+                pop.code = rset.getString("country.code");
+                population.add(pop);
+            }
+            return population;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get Population details");
+            return null;
+        }
+    }
+
+    /**
+     * Prints a list of Populations.
+     *
+     * @param population The list of Population to print.
+     */
+    public void printCountryPopulation(ArrayList<Country> population) {
+
+        // Check employees is not null
+        if (population == null)
+        {
+            System.out.println("No Country Population");
+            return;
+        }
+
+        // Print  header
+        System.out.println(String.format("%-20s ", "All the countries in the world organised by largest population to smallest."));
+        System.out.println(String.format("%-20s ", " "));
+        System.out.println(String.format("%-10s %10s %-50s %-30s %-30s %-30s", "Code", "Population", "Country", "Capital", "Continent", "Region"));
+        // Loop over all Retrieved Populations in the list
+        for (Country pop : population) {
+
+            if (pop == null)
+                continue;
+
+            String popCount = String.format("%-10s %10s %-50s %-30s %-30s %-30s", pop.code, pop.population, pop.name, pop.capital, pop.continent, pop.region);
+            System.out.println(popCount);
+        }
+
+
+    }
+
+
+    /**
+     * Gets the population of all countries in a continent.
+     *
+     * @return A list of all countries in continenet population sorted in descending order, or null if there is an error.
+     */
+    public ArrayList<Country> getContinentPopulation(String cont1) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT code, name, continent, region, capital, population "
+                            + "FROM country "
+                            + "WHERE continent = '"+ cont1 + "' ORDER BY population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract Population information
+            ArrayList<Country> population2 = new ArrayList<Country>();
+            while (rset.next()) {
+                Country pop = new Country();
+                pop.population = rset.getInt("country.population");
+                pop.name = rset.getString("country.Name");
+                pop.continent = rset.getString("country.continent");
+                pop.capital = rset.getInt("country.capital");
+                pop.region = rset.getString("country.region");
+                pop.code = rset.getString("country.code");
+                population2.add(pop);
+            }
+            return population2;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get Population details");
+            return null;
+        }
+    }
+
+    /**
+     * Prints a list of Countries in Continent Populations.
+     *
+     * @param population2 The list of Countries in Continent Populations to print.
+     */
+    public void printContinentPopulation(ArrayList<Country> population2) {
+        // Check Population is not null
+        if (population2 == null)
+        {
+            System.out.println("No Continent Population");
+            return;
+        }
+        // Print header
+        System.out.println(String.format("%-20s ", " "));
+        System.out.println(String.format("%-20s ", "Population of All the countries in a continent organised by largest population to smallest."));
+        System.out.println(String.format("%-20s ", " "));
+        System.out.println(String.format("%-10s %10s %-50s %-10s %-30s %-30s", "Code", "Population", "Country", "Capital", "Continent", "Region"));
+        // Loop over all Retrieved Populations in the list
+        // Check if query returned values.
+        for (Country pop : population2) {
+
+            if (pop == null)
+                continue;
+
+            String popCount = String.format("%-10s %10s %-50s %10s %-30s %-30s", pop.code, pop.population, pop.name, pop.capital, pop.continent, pop.region);
+            System.out.println(popCount);
+        }
+    }
+
+
+
+
+    /**
+     * Gets the population of all countries in a Region.
+     *
+     * @return A list of the population of all countries in a Region  sorted in descending order, or null if there is an error.
+     */
+
+    public ArrayList<Country> getRegionPopulation(String reg1) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT code, name, continent, region, capital, population "
+                            + "FROM country "
+                            + "WHERE region = '" + reg1 + "'Order By population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract Population information
+            ArrayList<Country> population3 = new ArrayList<Country>();
+            while (rset.next()) {
+                Country pop = new Country();
+                pop.population = rset.getInt("country.population");
+                pop.name = rset.getString("country.Name");
+                pop.continent = rset.getString("country.continent");
+                pop.capital = rset.getInt("country.capital");
+                pop.region = rset.getString("country.region");
+                pop.code = rset.getString("country.code");
+                population3.add(pop);
+            }
+            return population3;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get Population details");
+            return null;
+        }
+    }
+
+
+    /**
+     * Prints a list of Countries in a Region Populations.
+     *
+     * @param population3 The list of Countries in Continent Populations to print.
+     */
+    public void printRegionPopulation(ArrayList<Country> population3) {
+        // Check Population is not null
+        if (population3 == null)
+        {
+            System.out.println("No Region Population");
+            return;
+        }
+        // Print header
+        System.out.println(String.format("%-20s ", " "));
+        System.out.println(String.format("%-20s ", "Population of All the countries in a Region organised by largest population to smallest."));
+        System.out.println(String.format("%-20s ", " "));
+        System.out.println(String.format("%-10s %10s %-50s %-30s %-30s %-30s", "Code", "Population", "Country", "Capital", "Continent", "Region"));
+        // Loop over all Retrieved Populations in the list
+        // Check if query returned values.
+        for (Country pop : population3) {
+            if (pop == null)
+                continue;
+
+            String popCount = String.format("%-10s %10s %-50s %-30s %-30s %-30s", pop.code, pop.population, pop.name, pop.capital, pop.continent, pop.region);
+            System.out.println(popCount);
+        }
+    }
+
 
     /**
      * Gets top N populated countries in the world where N is provided by the user.
--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 
+
+
 public class AppTest {
 
     static App app;
@@ -14,6 +16,119 @@ public class AppTest {
     {
         app = new App();
     }
+
+
+    // Unit Testing for All the countries in the world population
+    @Test
+    void printCountryPopulationTestNull()
+    {
+        app.printCountryPopulation(null);
+    }
+
+    @Test
+    void printCountryPopulationTestContainsNull()
+    {
+        ArrayList<Country> population = new ArrayList<Country>();
+        population.add(null);
+        app.printCountryPopulation(population);
+    }
+
+    @Test
+    void printCountryPopulationTestEmpty()
+    {
+        ArrayList<Country> population = new ArrayList<Country>();
+        app.printCountryPopulation(population);
+    }
+    @Test
+    void printCountryPopulation()
+    {
+        ArrayList<Country> population = new ArrayList<Country>();
+        Country pop = new Country();
+        pop.code = "ABW";
+        pop.population = 103000;
+        pop.name = "Aruba";
+        pop.capital = 129;
+        pop.continent = "North America";
+        pop.region = "Caribbean";
+        population.add(pop);
+        app.printCountryPopulation(population);
+    }
+
+    // Unit Testing for All the Countries in the Continent Population
+    @Test
+    void printContinentPopulationTestNull()
+    {
+        app.printContinentPopulation(null);
+    }
+
+    @Test
+    void printContinentPopulationTestContainsNull()
+    {
+        ArrayList<Country> population2 = new ArrayList<Country>();
+        population2.add(null);
+        app.printContinentPopulation(population2);
+    }
+
+    @Test
+    void printContinentPopulationTestEmpty()
+    {
+        ArrayList<Country> population2 = new ArrayList<Country>();
+        app.printContinentPopulation(population2);
+    }
+
+    @Test
+    void printContinentPopulation()
+    {
+        ArrayList<Country> population2 = new ArrayList<Country>();
+        Country pop = new Country();
+        pop.code = "ABW";
+        pop.population = 103000;
+        pop.name = "Aruba";
+        pop.capital = 129;
+        pop.continent = "North America";
+        pop.region = "Caribbean";
+        population2.add(pop);
+        app.printContinentPopulation(population2);
+    }
+
+
+    // Unit Testing for All the countries in the Region population
+    @Test
+    void printRegionPopulationTestNull()
+    {
+        app.printRegionPopulation(null);
+    }
+
+    @Test
+    void printRegionPopulationTestContainsNull()
+    {
+        ArrayList<Country> population3 = new ArrayList<Country>();
+        population3.add(null);
+        app.printRegionPopulation(population3);
+    }
+
+    @Test
+    void printRegionPopulationTestEmpty()
+    {
+        ArrayList<Country> population3 = new ArrayList<Country>();
+        app.printRegionPopulation(population3);
+    }
+
+    @Test
+    void printRegionPopulation()
+    {
+        ArrayList<Country> population3 = new ArrayList<Country>();
+        Country pop = new Country();
+        pop.code = "ABW";
+        pop.population = 103000;
+        pop.name = "Aruba";
+        pop.capital = 129;
+        pop.continent = "North America";
+        pop.region = "Caribbean";
+        population3.add(pop);
+        app.printRegionPopulation(population3);
+    }
+
 
 
     //Unit Testing for Top N Populated Countries in the World where N was provided
